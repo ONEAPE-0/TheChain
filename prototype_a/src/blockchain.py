41964db6a718a4ab7b5978577979b38769b96610
NSG <nsg@oneape.xyz>
2024-08-16 17:19:53 -0400

    Set API Boilerplate

--- prototype_a/src/blockchain.py
+++ prototype_a/src/blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+from flask import Flask, jsonify
 
 class Blockchain(object):
     def __init__(self):
@@ -99,3 +102,33 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+
+# Instantiate our node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new Transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
